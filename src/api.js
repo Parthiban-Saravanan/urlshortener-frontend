ad2668c91e58311013df2851b239d1cee874e2a7
Parthiban <137680054+Parthiban-Saravanan@users.noreply.github.com>
2024-08-12 10:40:24 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000', // Replace with your backend URL
+    baseURL: 'https://urlshortener-backend-gxya.onrender.com', // Replace with your backend URL
     headers: {
         'Content-Type': 'application/json',
     },
